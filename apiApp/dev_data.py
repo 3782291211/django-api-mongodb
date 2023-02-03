e668399d430af434e9724d08018196dafa100bb1
3782291211 <Laklouk98@hotmail.com>
2023-02-03 21:50:51 +0000

    Adding dev data for Mongo db

---
+++ apiApp/dev_data.py
@@ -0,0 +1,146 @@
+from datetime import datetime
+
+patterns = [
+    {
+        "username": "john123",
+        "pattern_name": "orphous",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "john123",
+        "pattern_name": "mango",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "bob12",
+        "pattern_name": "bricklayer",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "bob12",
+        "pattern_name": "layer",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "andy09",
+        "pattern_name": "bluegrass",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "zack81",
+        "pattern_name": "fisherman",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "mark123",
+        "pattern_name": "bendable",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+        {
+        "username": "john123",
+        "pattern_name": "rugged",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "john123",
+        "pattern_name": "jumpy",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "bob12",
+        "pattern_name": "doubtful",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "bob12",
+        "pattern_name": "superfluous",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "andy09",
+        "pattern_name": "bulge",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "zack81",
+        "pattern_name": "ruin",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "mark123",
+        "pattern_name": "bunk",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "zack81",
+        "pattern_name": "pyramid stalker",
+        "pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111010000 0100101010 0000101010",
+        "created_at": datetime.now(),
+    },
+]
+
+
+users = [
+    {
+        "account_owner": "John Smith",
+        "username": "john123",
+        "email": "john132@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Andy Jones",
+        "username": "andy09",
+        "email": "andy09@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Bob Job",
+        "username": "bob12",
+        "email": "bob12@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Zack Roman",
+        "username": "zack81",
+        "email": "zack81@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Mark iphone",
+        "username": "mark123",
+        "email": "mark123@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+     {
+        "account_owner": "Jung Blandy",
+        "username": "fluffyscalp",
+        "email": "berdy123@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Brig Lawn",
+        "username": "lornBG",
+        "email": "blawn1923@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
+    {
+        "account_owner": "Brick man",
+        "username": "BM",
+        "email": "bricklayer1923@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    }
+]
