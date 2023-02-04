0305b981f191b4a1a436f8fccba16dbc3bce8dd7
3782291211 <Laklouk98@hotmail.com>
2023-02-04 01:48:04 +0000

    Adding url paths for endpoints

---
+++ apiApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.api_endpoints),
+    path("api/patterns", views.get_patterns, name="get_patterns"),
+    path("api/patterns/<str:id>", views.single_pattern, name="get_single_pattern"),
+    path("api/users", views.get_users, name="get_users"),
+    path("api/users/<str:id>", views.get_single_user, name="get_single_user"),
+    path('api/users/<str:username>/patterns',views.get_patterns_by_username, name='get_patterns_by_username'),
+    path("api/auth", views.get_test, name='auth_get_test')
+]
