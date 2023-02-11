b0e5366c22315079e0aa140d51d19ba049407a7b
3782291211 <Laklouk98@hotmail.com>
2023-02-11 18:09:08 +0000

    Adding paths for new endpoint (comments)

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("api/users", views.get_users, name="get_users"),
     path("api/users/<str:id>", views.get_single_user, name="get_single_user"),
     path('api/users/<str:username>/patterns',views.get_patterns_by_username, name='get_patterns_by_username'),
-    path("api/auth", views.get_test, name='auth_get_test')
+    path("api/auth", views.get_test, name='auth_get_test'),
+    path("api/comments", views.get_comments, name="get_comments"),
+    path("api/comments/<str:id>", views.single_comment, name="delete_single_pattern"),
 ]
